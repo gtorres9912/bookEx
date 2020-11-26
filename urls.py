a8c9974853e193764095ca4e5da3831e16c1bb59
gtorres9912 <gtorres9912@gmail.com>
2020-11-25 16:31:14 -0800

    update

--- urls.py
+++ urls.py
@@ -15,7 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
+
+from django.views.generic.base import TemplateView
+from bookMng.views import Register
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/success', TemplateView.as_view(template_name="registration/register_success.html"),
+         name='register-success'),
+    path('register', Register.as_view(), name='register'),
+    path('', include('django.contrib.auth.urls')),
+    path('', include('bookMng.urls')),
 ]
